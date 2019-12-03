33185ff6f761b65a5dc53316bfb65f2b5899f146
kobe_dream <1228830203@qq.com>
2019-12-03 13:51:33 +0800

    添加sdk文件

---
+++ Example/Pods/Target Support Files/Pods-RYXAuthID_Tests/Pods-RYXAuthID_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RYXAuthID_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RYXAuthID_Tests
+@end
---
+++ Example/Pods/Target Support Files/RYXAuthID/RYXAuthID-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RYXAuthID : NSObject
+@end
+@implementation PodsDummy_RYXAuthID
+@end
